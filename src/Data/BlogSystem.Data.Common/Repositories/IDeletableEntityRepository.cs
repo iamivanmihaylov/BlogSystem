aebe53ffb789179d950c0b0d5cd1b62bb1ff305c
Ivan Mihaylov <ivan4o03.it@gmail.com>
2023-12-23 19:41:08 +0200

    Initial Commit

---
+++ src/Data/BlogSystem.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace BlogSystem.Data.Common.Repositories
+{
+    using System.Linq;
+
+    using BlogSystem.Data.Common.Models;
+
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
